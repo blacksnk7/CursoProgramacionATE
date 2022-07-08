1d1737976ebf823c6ae2af7f9564931974d7d6e9
Sebastian Ferro <ketokioto@gmail.com>
2022-07-08 16:05:12 -0300

    Finished steps 3 and 4 of the TP1.

--- TP1/functions.py
+++ TP1/functions.py
@@ -2,6 +2,9 @@ import os
 import string as st
 
 def read_text():
+    '''
+    This function reads a text from console and returns the text as a single string.
+    '''
     text = ''
     line = input('Ingrese lineas de texto. Para terminar, ingrese la linea "@fin"\n').lower()
     while ('@fin' not in line):
@@ -11,6 +14,31 @@ def read_text():
     if (len(line) != 0):
         text += line
     return text
+
+def read_file(file_name):
+    '''
+    This function reads a text file and converts it into lower case, removes any tildes and any punctuation symbols.
+    It then checks how many words inside the file are missing in the "spanish.lst" file and returns a list with
+    all missing words.
+    '''
+    path = os.path.join(os.getcwd(), file_name)
+    non_spanish = []
+    try:
+        file = open(path, mode="r", encoding="utf-8")
+        end = False
+    except FileNotFoundError:
+        print(f'El archivo "{file_name}" no existe')
+        end = True
+    except:
+        print('Error inesperado')
+        end = True
+    if (not end):
+        text = file.read().lower()
+        text = remove_tilde(text)
+        text = remove_punctuation(text)
+        non_spanish = in_spanish_no_duplicates(text)
+    file.close()
+    return non_spanish
 
 def in_spanish_no_duplicates(text):
     '''
@@ -32,8 +60,13 @@ def in_spanish_no_duplicates(text):
     for elem in non_spanish:
         print(f'La palabra "{elem}" no se encuentra dentro del archivo de palabras.')
     print(f'En total, {len(non_spanish)} palabras no se encontraron dentro del archivo.')
+    file.close()
+    return non_spanish
 
 def remove_tilde(text):
+    '''
+    This function removes any tildes from all vocals inside the text.
+    '''
     tilde = 'áéíóú'
     no_tilde = 'aeiou'
     for elem in range(len(tilde)):
@@ -41,7 +74,61 @@ def remove_tilde(text):
     return text
 
 def remove_punctuation(text):
+    '''
+    This function removes any puntuation symbols from the text.
+    '''
     punct = st.punctuation
     for elem in punct:
         text = text.replace(elem, '')
     return text
+
+def add_to_dict(file_name, word):
+    '''
+    This function adds the string "word" into a file with the name "file_name".
+    '''
+    path = os.path.join(os.getcwd(), file_name)
+    try:
+        file = open(path, mode="a", encoding="utf-8")
+    except FileNotFoundError:
+        return print(f'El archivo "{path}" no existe')
+    except:
+        return print('Error inesperado.')
+    file.write(word)
+    file.close()
+
+def in_dictionary(file_name, word):
+    '''
+    This functions checks if the string "word" is found inside the file "file_name"
+    '''
+    path = os.path.join(os.getcwd(), file_name)
+    try:
+        file = open(path, mode="r", encoding="utf-8")
+        spanish_words = file.read()
+    except FileNotFoundError:
+        return print(f'El archivo "{path}" no existe')
+    except Exception as exception:
+        return print(exception)
+    if (word in spanish_words):
+        file.close()
+        return True
+    else:
+        file.close()
+        return False
+
+def replace_in_textfile(file_name, old_word, new_word):
+    '''
+    This function replaces the string "old_word" for "new_word" inside the file "file_name". If the "old_word" doesnt exist then no changes are
+    made.
+    '''
+    path = os.path.join(os.getcwd(), file_name)
+    try:
+        file = open(path, mode="r+", encoding="utf-8")
+        old_text = file.read()
+    except FileNotFoundError:
+        return print(f'El archivo "{path}" no existe')
+    except:
+        return print('Error inesperado.')
+    new_text = old_text.replace(old_word, new_word)
+    file.seek(0)
+    file.write(new_text)
+    file.close()
--- TP1/main.py
+++ TP1/main.py
@@ -1,29 +1,38 @@
 import os
+import sys
 import functions as func
 
-option = input('Elija 1 o 2 segun prefiera: \n1 - Ingresar el texto por pantalla. \n2 - Ingresar el nombre de un archivo de texto (el archivo se debe encontrar dentro de la carpeta "main" o se producira una excepcion). \n')
-while (option != '1') and (option != '2'):
-    print('Opcion no valida. Seleccione nuevamente.')
+if (len(sys.argv) == 1):
     option = input('Elija 1 o 2 segun prefiera: \n1 - Ingresar el texto por pantalla. \n2 - Ingresar el nombre de un archivo de texto (el archivo se debe encontrar dentro de la carpeta "main" o se producira una excepcion). \n')
-if (option == '1'):
-    text = func.read_text()
-    text = func.remove_tilde(text)
-    text = func.remove_punctuation(text)
-    func.in_spanish_no_duplicates(text)
-else:
-    file_name = input('Ingrese el nombre del archivo con la extension (ej: texto.txt): ')
-    path = os.path.join(os.getcwd(), file_name)
-    try:
-        file = open(path, mode="r", encoding="utf-8")
-        end = False
-    except FileNotFoundError:
-        print(f'El archivo "{file_name}" no existe')
-        end = True
-    except:
-        print('Error inesperado')
-        end = True
-    if (not end):
-        text = file.read().lower()
+    while (option != '1') and (option != '2'):
+        print('Opcion no valida. Seleccione nuevamente.')
+        option = input('Elija 1 o 2 segun prefiera: \n1 - Ingresar el texto por pantalla. \n2 - Ingresar el nombre de un archivo de texto (el archivo se debe encontrar dentro de la carpeta "main" o se producira una excepcion). \n')
+    if (option == '1'):
+        text = func.read_text()
         text = func.remove_tilde(text)
         text = func.remove_punctuation(text)
-        func.in_spanish_no_duplicates(text)
+        wrong_words = func.in_spanish_no_duplicates(text)
+    else:
+        file_name = input('Ingrese el nombre del archivo con la extension (ej: texto.txt): ')
+        wrong_words = func.read_file(file_name)
+else:
+    file_name = sys.argv[1]
+    wrong_words = func.read_file(file_name)
+if (len(wrong_words) > 0):
+    dict_name = 'spanish.lst'
+    for elem in wrong_words:
+        choice = input(f'Elija una opcion: \n1 - Agregar la palabra "{elem}" al archivo {dict_name}.\n2 - Corregir la palabra "{elem}".\n3 - Ignorar.\n4 - Salir.\n')
+        while (choice not in ['1', '2', '3', '4']):
+            print('Error. Opcion no permitiva. Ingrese nuevamente.')
+            choice = input(f'Elija una opcion: \n1 - Agregar la palabra "{elem}" al archivo {dict_name}.\n2 - Corregir la palabra "{elem}".\n3 - Ignorar.\n4 - Salir.\n')
+        match choice:
+            case '1':
+                func.add_to_dict(dict_name, elem)
+            case '2':
+                new_word = input(f'Ingrese una palabra para reemplazar "{elem}": ')
+                while (not func.in_dictionary(dict_name, new_word)):
+                    new_word = input(f'La palabra {new_word} no se encuentra dentro del diccionario. Ingrese otra palabra: ')
+                if ((len(sys.argv) > 1) or (option == '2')):
+                    func.replace_in_textfile(file_name, elem, new_word)
+            case '4':
+                break
